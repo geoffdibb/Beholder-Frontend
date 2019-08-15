4cece253cec988077fc2e09b8ae87b79194f911b
lushilling <lushilling@hotmail.co.uk>
2019-08-15 14:05:19 +0100

    made js files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
 import './App.css';
+import NavigationBar from './Components/NavigationBar.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default class App extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <Router>
+          <NavigationBar />
+        </Router>
+      </div>
+    )
+  }
 }
-
-export default App;
---
+++ src/Components/Profile.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class Map extends React.Component {
+
+    render() {
+        return (
+            <div>
+            </div>
+        );
+    };
+
+}
