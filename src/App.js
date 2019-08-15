f31109c2157f705ffb945d876156824a97479c1e
lushilling <lushilling@hotmail.co.uk>
2019-08-15 14:23:56 +0100

    routing

--- src/App.js
+++ src/App.js
@@ -3,7 +3,13 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import './App.css';
 import NavigationBar from './Components/NavigationBar.js';
-
+import Homepage from './Components/Homepage';
+import Login from './Components/Login';
+import Map from './Components/Map';
+import Profile from './Components/Profile';
+import ResultPage from './Components/ResultPage';
+import SearchPage from './Components/SearchPage';
+import Audit from './Components/Audit';
 
 export default class App extends React.Component {
 
@@ -12,6 +18,21 @@ export default class App extends React.Component {
       <div>
         <Router>
           <NavigationBar />
+
+          <Route exact path="/" component={Homepage} />
+
+          <Route path="/Login" component={Login} />
+
+          <Route path="/Map" component={Map} />
+
+          <Route path="/Profile" component={Profile} />
+
+          <Route path="/ResultPage" component={ResultPage} />
+
+          <Route path="/SearchPage" component={SearchPage} />
+
+          <Route path="/Audit" component={Audit} />
+          
         </Router>
       </div>
     )
