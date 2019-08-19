46af9a980709d9c58fd3da52327fb18a36ad4a23
Chris-McQuiggan <chris@mcquiggan.info>
2019-08-19 17:15:58 +0100

    centered login

--- src/Components/NavigationBar.js
+++ src/Components/NavigationBar.js
@@ -9,6 +9,7 @@ import {
     NavLink
 } from 'reactstrap';
 
+
 export default class NavigationBar extends React.Component {
     constructor(props) {
         super(props);
@@ -27,23 +28,23 @@ export default class NavigationBar extends React.Component {
         return (
             <div>
                 <Navbar color="dark" dark expand="md">
-                    <NavbarBrand className="mx-auto d-none" href="/" >
-                        <img
-                            src="../../logo192.png"
-                            width="30"
-                            height="30"
-                            alt="React Bootstrap logo"
-                        />
-                        Beholder</NavbarBrand>
+                    <Nav className="ml-auto" navbar>
+                        <NavItem>
+                            <NavLink href="./SearchPage">Home</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <div></div>
+                        </NavItem>
+                    </Nav>
                     <NavbarToggler onClick={this.toggle} />
                     <Collapse isOpen={this.state.isOpen} navbar>
-                    <NavbarBrand className="ml-auto d-lg-block-auto" href="/" >
-                        <img
-                            src="../../LucyVariant.png"
-                            width="160"
-                            height="39"
-                            alt="React Bootstrap logo"
-                        />
+                        <NavbarBrand className="ml-auto" href="/" >
+                            <img
+                                src="../../LucyVariant.png"
+                                width="160"
+                                height="39"
+                                alt="React Bootstrap logo"
+                            />
                         </NavbarBrand>
                         <Nav className="ml-auto" navbar>
                             <NavItem>
