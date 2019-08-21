98f1695792c0696ccb744f6cb2c0394008d838a8
geoffdibb <geoffdibb@yahoo.co.uk>
2019-08-21 11:53:00 +0100

    Added axios function on searchpage linking to endpoint

--- src/Components/Content/Search/SearchForm.js
+++ src/Components/Content/Search/SearchForm.js
@@ -1,7 +1,47 @@
 import React from 'react';
 import { FormGroup,Button, Label, Input } from 'reactstrap';
+import axios from "axios";
+
 
 export default class SearchForm extends React.Component {
+
+
+
+    constructor() {
+        super();
+        this.state = {
+            responseData: ""
+        };
+    }
+// searchdb = ()=>{
+//     console.log("Childfunctioncalled");
+// }
+searchdb =(e) => {
+        e.preventDefault();
+        let name = e.target[0].value;
+                if (name === "") {
+            this.setState({ message: "Please fill all fields" })
+        } else {
+            axios
+                .get("http://localhost:8080/search/name/" + name)
+                .then(response => {
+                   this.setState({
+                    responseData: (response.data)
+                });
+                    this.props.passedFunction();
+                })
+                .catch(err => { this.setState({ message: "name not found" }); });
+
+        }
+    }
+componentDidMount() {
+    this.props.onRef(this)
+  }
+  componentWillUnmount() {
+    this.props.onRef(undefined)
+  }
+
+
 
 
     render() {
@@ -9,7 +49,7 @@ export default class SearchForm extends React.Component {
             <div>
             <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
                 <FormGroup>
-                    <Label for="Catagory" hidden>Catagoy</Label>
+                    <Label for="Category" hidden>Category</Label>
                     <Input type="select">
                         <option>Name</option>
                         <option>Location</option>
@@ -22,7 +62,9 @@ export default class SearchForm extends React.Component {
                 <FormGroup>
                     <Label for="Search" hidden>Search</Label>
                     <Input type="search" name="search" id="Search" placeholder="Search" />
+
                 </FormGroup>
+
             </div>
             <div>
                   {/* <Button variant="Add Search Field">Add Search Field</Button> */}
