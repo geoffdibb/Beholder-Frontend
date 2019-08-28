81285d65b041e53863f02cde9c63fd4693b41db8
Chris-McQuiggan <chris@mcquiggan.info>
2019-08-28 14:50:03 +0100

    fix

--- src/Components/Content/Search/Results/ResultList.js
+++ src/Components/Content/Search/Results/ResultList.js
@@ -54,7 +54,7 @@ export default class ResultList extends React.Component {
         if (this.props.category === "Forenames" || this.props.category === "Surname") {
             list = <div>
                 {this.props.searchResults.map(Result => (
-                    <ResultPanel
+                    <ResultPanelCitizen
                         key={Result.index}
                         forenames={Result.forenames}
                         surname={Result.surname}
